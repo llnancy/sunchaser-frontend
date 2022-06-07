eb85b94ee1aa992ba277e135ffa32344f35ca94a
lilu <admin@lilu.org.cn>
2022-06-07 23:53:07 +0800

    add props、life cycle

--- comment-app/src/comment.js
+++ comment-app/src/comment.js
@@ -8,10 +8,16 @@ class Comment extends React.Component {
       { id: 1, name: "Jack", content: "沙发！" },
       { id: 2, name: "Tom", content: "一楼" },
       { id: 3, name: "Alice", content: "666" }
-    ]
+    ],
+
+    // 昵称
+    userName: '',
+    // 评论内容
+    content: ''
   }
 
   render() {
+    const { userName, content } = this.state;
     return (
       <div className="app">
         <div>
@@ -19,14 +25,20 @@ class Comment extends React.Component {
             className="user"
             type="text"
             placeholder="请输入昵称"
+            value={userName}
+            name="userName"
+            onChange={this.handleForm}
           /><br />
           <textarea
             className="content"
             cols="30"
             rows="10"
             placeholder="大侠请留步，留下评论再走也不迟~"
+            value={content}
+            name="content"
+            onChange={this.handleForm}
           /><br />
-          <button>发表评论</button>
+          <button onClick={this.addComment}>发表评论</button>
         </div>
 
         {this.renderList()}
@@ -35,7 +47,10 @@ class Comment extends React.Component {
   }
 
   renderList() {
+    // 解构赋值
     const { comments } = this.state;
+
+    // 条件渲染
     if (comments.length === 0) {
       return <div className="no-comment">暂无评论，快去占沙发吧~</div>;
     }
@@ -52,6 +67,38 @@ class Comment extends React.Component {
         }
       </ul>
     )
+  }
+
+  handleForm = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  addComment = () => {
+    const { comments, userName, content } = this.state;
+
+    // 非空检验
+    if (userName.trim() === '' || content.trim() === '') {
+      alert("请输入昵称或评论内容！");
+      return;
+    }
+
+    const newComments = [
+      {
+        id: Math.random(),
+        name: userName,
+        content: content
+      },
+      ...comments
+    ];
+    this.setState({
+      comments: newComments,
+      // 评论添加完成后清空昵称和评论内容
+      userName: '',
+      content: ''
+    })
   }
 }
 
--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Hello from './component/HelloComponent';
+import PropTypes from 'prop-types';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -325,6 +326,472 @@ class FormRef extends React.Component {
   }
 }
 root.render(<FormRef />);
+
+
+/**
+ * props
+ * 函数组件传递接收props数据
+ * 
+ * @param {*} props 
+ * @returns HelloFunProps
+ */
+const HelloPropsFun = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>props: {props.name}</h1>
+    </div>
+  );
+}
+root.render(<HelloPropsFun name="SunChaser" age={19} />)
+
+/**
+ * 类组件传递接收props数据
+ */
+class HelloPropsClass extends React.Component {
+
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h1>props: {this.props.name}</h1>
+      </div>
+    );
+  }
+}
+root.render(<HelloPropsClass name="SunChaser" />)
+
+/**
+ * props特性：
+ * 1. props能传递任意类型的数据。
+ * 2. props是只读属性，无法进行修改。
+ * 3. 类组件中如果有构造函数constructor()，应该将props传递给super()，否则在构造函数中无法获取到props。
+ * 
+ * 函数组件接收props前面不加this，类组件接收props前面需要加this。
+ */
+class PropsTypeClass extends React.Component {
+
+  constructor(props) {
+    super(props);
+    console.log("constructor: ");
+    console.log(this.props);
+  }
+
+  render() {
+    console.log("render: ");
+    console.log(this.props);
+    // 调用传递进来的方法
+    this.props.fun();
+    return (
+      <div>
+        <h1>props: {this.props.name}</h1>
+        {/* 渲染传递进来的JSX */}
+        {this.props.tag}
+      </div>
+    );
+  }
+}
+root.render(
+  <PropsTypeClass
+    name="SunChaser"
+    age={19}
+    colors={['red', 'green', 'blue']}
+    fun={() => console.log("props传递一个函数")}
+    tag={<p>props传递JSX</p>}
+  />
+)
+
+/**
+ * 组件通讯之父组件传值给子组件
+ */
+class ParentComponent extends React.Component {
+
+  state = {
+    lastName: "王"
+  }
+
+  render() {
+    return (
+      <div>
+        父组件传值给子组件：<ChildrenComponent lastName={this.state.lastName} />
+      </div>
+    );
+  }
+}
+
+class ChildrenComponent extends React.Component {
+
+  render() {
+    return (
+      <div>子组件接收到数据：{this.props.lastName}</div>
+    );
+  }
+}
+
+root.render(<ParentComponent />);
+
+/**
+ * 组件通讯之子组件传值给父组件
+ */
+class ParentComponent2 extends React.Component {
+
+  state = {
+    parentData: ''
+  }
+
+  // 提供回调函数（接收子组件传递的数据）
+  getChildData = (data) => {
+    console.log("接收到子组件传递的数据：" + data);
+    this.setState({
+      parentData: data
+    })
+  }
+
+  render() {
+    return (
+      <div>父组件：{this.state.parentData}
+        <ChildrenComponent2 getData={this.getChildData} />
+      </div>
+    );
+  }
+}
+
+class ChildrenComponent2 extends React.Component {
+
+  state = {
+    childData: '子组件的数据childData'
+  }
+
+  render() {
+    return (
+      <div>子组件：<button onClick={this.handleClick}>点击传值给父组件</button></div>
+    );
+  }
+
+  handleClick = () => {
+    this.props.getData(this.state.childData);
+  }
+}
+root.render(<ParentComponent2 />)
+
+/**
+ * 组件通讯之兄弟组件传值
+ * 
+ * 将共享状态提升到最近的公共父组件中，由公共父组件管理这个状态。
+ * 父组件职责：1. 提供共享状态。2. 提供操作共享状态的方法。
+ * 子组件通过props接收共享状态的值或操作共享状态的方法。
+ */
+class Counter extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+  render() {
+    return (
+      <div>
+        <Child1 count={this.state.count} />
+        <Child2 onIncrement={this.onIncrement} />
+      </div>
+    );
+  }
+
+  onIncrement = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+}
+
+const Child1 = (props) => {
+  return <h1>计数器：{props.count}</h1>
+}
+
+const Child2 = (props) => {
+  // 调用props传递进来的方法
+  return <button onClick={() => props.onIncrement()}>+1</button>
+}
+root.render(<Counter />)
+
+/**
+ * Context: 跨组件传递数据
+ * 
+ * 使用步骤：
+ * 1. 调用React.createContext()创建Provider（提供数据）和Consumer（消费数据）两个组件。
+ * 2. 使用Provider组件作为父节点。
+ * 3. 给Provider组件设置value属性，表示要传递的数据。
+ * 4. 调用Consumer组件接收数据。
+ */
+
+const { Provider, Consumer } = React.createContext();
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <Provider value="SunChaser">
+        <div>
+          <Node />
+        </div>
+      </Provider>
+    );
+  }
+}
+
+class Node extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <SubNode />
+      </div>
+    );
+  }
+}
+
+const SubNode = () => {
+  return (
+    <div>
+      <Child />
+    </div>
+  );
+}
+
+const Child = () => {
+  return (
+    <div>
+      child组件
+      <Consumer>
+        {
+          data => <span>{data}</span>
+        }
+      </Consumer>
+    </div>
+  );
+}
+root.render(<App />);
+
+/**
+ * 组件标签的子节点：props.children
+ * children属性：当组件标签有子节点时，props自动有children属性。
+ * 和普通props属性一样，children可以为文本、React元素、组件及函数等任意值。
+ */
+class PropsChildren extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>组件标签的子节点:props.children</h1>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+root.render(
+  <PropsChildren>
+    <div>组件子节点</div>
+  </PropsChildren>
+);
+
+/**
+ * props校验。
+ * 需要安装prop-types依赖（npm install prop-types）
+ */
+class PropsChecked extends React.Component {
+
+  render() {
+    return (
+      <div>
+        props checked
+        <ul>
+          {this.props.colors.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+// 常见类型: array、bool、func、number、object、string
+// React元素类型：element
+// 必填项：isRequired
+// 特定结构的对象：shape({ })
+// https://reactjs.org/docs/typechecking-with-proptypes.html
+PropsChecked.propTypes = {
+  colors: PropTypes.array.isRequired,
+  age: PropTypes.number,
+  doSomething: PropTypes.func.isRequired,
+  filter: PropTypes.shape({
+    area: PropTypes.string,
+    price: PropTypes.number
+  })
+}
+root.render(<PropsChecked colors={["red", "blue"]} doSomething={() => { }} />)
+
+/**
+ * props默认值
+ */
+class DefaultValue extends React.Component {
+
+  render() {
+    return (
+      <div>
+        props默认值:{this.props.size}
+      </div>
+    );
+  }
+}
+
+DefaultValue.defaultProps = {
+  size: 10
+}
+root.render(<DefaultValue />);
+
+/**
+ * 类组件生命周期
+ */
+
+/**
+ * 1. 创建时（挂载阶段）
+ * constructor：创建组件时触发。初始化state，为事件处理程序绑定this指向。
+ * render：每次组件渲染都会执行。渲染UI。
+ * 钩子函数componentDidMount：组件挂载（完成DOM渲染）后触发。操作DOM；发送网络请求。
+ * 
+ * 执行顺序：constructor -> render -> componentDidMount
+ */
+class CreatingLifeCycle extends React.Component {
+
+  constructor() {
+    super();
+    console.warn("生命周期钩子函数:constructor");
+  }
+
+  componentDidMount() {
+    console.warn("生命周期钩子函数:componentDidMount");
+  }
+
+  render() {
+    console.warn("生命周期钩子函数:render");
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+root.render(<CreatingLifeCycle />);
+
+/**
+ * 2. 更新时（更新阶段）
+ * 执行时机：
+ * 1. setState()
+ * 2. forceUpdate()
+ * 3. 组件接收到新的props
+ * 4. 钩子函数componentDidUpdate
+ * 
+ * 执行顺序：render -> componentDidUpdate
+ */
+class UpdatingLifeCycle extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+  render() {
+    console.warn("生命周期钩子函数:render");
+    return (
+      <div>
+        <Count count={this.state.count} />
+        <button onClick={this.handleClick}>+1</button>
+      </div>
+    );
+  }
+
+  handleClick = () => {
+    // this.forceUpdate();
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+}
+
+class Count extends React.Component {
+
+  render() {
+    console.warn("子组件--生命周期钩子函数:render");
+    return (
+      <div id="count">count: {this.props.count}</div>
+    );
+  }
+
+  /**
+   * 调用setState更新状态或者发送网络请求操作必须放在一个if条件中
+   */
+  componentDidUpdate(preProps) {
+    console.warn("子组件--生命周期钩子函数:componentDidUpdate");
+    const count = document.getElementById("count");
+    console.log(count.innerHTML);
+
+    console.log("更新前的props", preProps, ", 当前的props", this.props);
+    if (preProps.count !== this.props.count) {// 避免递归更新
+      // this.setState({})
+      // 发送网络请求
+    }
+  }
+}
+root.render(<UpdatingLifeCycle />);
+
+/**
+ * 3. 卸载时（卸载阶段）
+ * 执行时机：组件从页面中消失。
+ * 钩子函数：componentWillUnmount
+ * 一般用来执行清理工作（例如清理定时器）。
+ */
+class UninstallingLifeCycle extends React.Component {
+
+  state = {
+    count: 0
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.count > 3 ? <p>加爆了</p> : <UninstallCount count={this.state.count} />
+        }
+        <button onClick={this.handleClick}>+1</button>
+      </div>
+    );
+  }
+
+  handleClick = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+}
+
+class UninstallCount extends React.Component {
+
+  componentDidMount() {
+    // 设置定时器
+    this.timerId = setInterval(() => {
+      console.log("定时器执行...")
+    }, 500);
+  }
+
+  render() {
+    console.warn("子组件--生命周期钩子函数:render");
+    return (
+      <div id="count">count: {this.props.count}</div>
+    );
+  }
+
+  componentWillUnmount() {
+    console.warn("子组件--生命周期钩子函数:componentWillUnmount");
+    // 清理定时器
+    clearInterval(this.timerId);
+  }
+}
+root.render(<UninstallingLifeCycle />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
